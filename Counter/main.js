e2243d0480823fdc8710d391cf42f8ae0beb3c34
ItsTheCreeper <38690339+ItsTheCreeper@users.noreply.github.com>
2018-05-22 21:30:18 +0800

    counter fix
    fixed the background and made the color of the number change everytime you press the button

--- Counter/main.js
+++ Counter/main.js
@@ -5,4 +5,20 @@ var output = document.getElementById("output");
 button.onclick = function () {
     count += 1;
     output.innerHTML = count;
+    output.style.color = generateRandomColor();
+}
+
+
+function generateRandomColor() {
+    var colors = [0, 0, 0]
+    for (var i = 0; i <= 2; i++) {
+        colors[i] = rndNumber();
+    }
+    console.log("rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")")
+    return "rgb(" + colors[0] + ", " + colors[1] + ", " + colors[2] + ")";
+}
+
+function rndNumber() {
+    var num = Math.floor(Math.random() * 256);
+    return num;
 }
